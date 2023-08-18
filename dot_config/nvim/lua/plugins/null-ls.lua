e0f0b895803c446584a9c5ff67bb3b9da192e1b8
Kerati <enderwolf50@gmail.com>
2023-08-18 14:59:29 +0800

    feat(neovim): add plugins

---
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,11 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    opts.sources = vim.list_extend(opts.sources, {
+      nls.builtins.formatting.ruff,
+      nls.builtins.formatting.black,
+      -- nls.builtins.diagnostics.ruff,
+    })
+  end,
+}
