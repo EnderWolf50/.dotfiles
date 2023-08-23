9308622a4ce39dfad5507bd404297b1e056d5882
Kerati <enderwolf50@gmail.com>
2023-08-23 22:21:04 +0800

    fix(neovim): disable LazyVim's prettier formatting
    LazyVim use prettierd but it cannot read the configuration file correctly.

--- dot_config/nvim/lua/plugins/null-ls.lua
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -3,6 +3,8 @@ return {
   opts = function(_, opts)
     local nls = require("null-ls")
     opts.sources = vim.list_extend(opts.sources, {
+      nls.builtins.formatting.prettier,
+
       nls.builtins.formatting.ruff,
       nls.builtins.formatting.black,
       -- nls.builtins.diagnostics.ruff,
