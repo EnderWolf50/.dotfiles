04c8d8410978a880a2cb9412df315cafe1b9032c
Kerati <enderwolf50@gmail.com>
2024-01-07 23:51:01 +0800

    fix(neovim): remap add cursor keys

--- dot_config/nvim/lua/plugins/vim-visual-multi.lua
+++ dot_config/nvim/lua/plugins/vim-visual-multi.lua
@@ -1,4 +1,11 @@
 return {
   "mg979/vim-visual-multi",
   branch = "master",
+  lazy = false,
+  init = function()
+    vim.g.VM_maps = {
+      ["Add Cursor Down"] = "<M-C-Down>",
+      ["Add Cursor Up"] = "<M-C-Up>",
+    }
+  end,
 }
